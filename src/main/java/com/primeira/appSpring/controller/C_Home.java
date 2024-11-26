bc52f08c0a9120de2c519e3dfc074f45a9f62cc9
Pipo <pietrocorona16@gmail.com>
2024-11-26 16:43:58 -0300

    fiz nada só vi se tava bagua concertei o btn de logout

--- src/main/java/com/primeira/appSpring/controller/C_Home.java
+++ src/main/java/com/primeira/appSpring/controller/C_Home.java
@@ -9,6 +9,8 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Controller
